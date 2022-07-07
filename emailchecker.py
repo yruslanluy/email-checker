00c406eb6b114128bd54805ab5ffaf8387cec7bb
yruslanluy <48959405+yruslanluy@users.noreply.github.com>
2022-07-07 19:30:52 +0300

    Update emailchecker.py

--- emailchecker.py
+++ emailchecker.py
@@ -2,8 +2,6 @@ import poplib
 
 emails = []
 i = 0
-# pop3server = 'pop.rambler.ru'
-# pop3server = poplib.POP3_SSL(pop3server)  # open connection
 
 with open("drafts.txt") as file:
 	for line in file:
@@ -18,24 +16,3 @@ with open("drafts.txt") as file:
 		print(i, line.rsplit(':')[0], ":", mail_count)
 		pop3server.quit()
 		i += 1
-
-# username = 'pavelsorokinwy3399@rambler.ru'
-# password = 'Scottsandra829'
-
-# print(pop3server.getwelcome())  # show welcome message
-
-# pop3info = pop3server.stat()  # access mailbox status
-# mail_count = pop3info[0]  # total email
-# print("Total no. of Email : ", mail_count)
-# print("\n\nStart Reading Messages\n\n")
-# for i in range(mail_count):
-# 	for message in pop3server.retr(i+1)[1]:
-# 		print(message)
-
-# pop3server.quit()
-
-
-#
-# with open("result.txt", "w") as file:
-# 	for line in emails:
-# 		file.write(line)
